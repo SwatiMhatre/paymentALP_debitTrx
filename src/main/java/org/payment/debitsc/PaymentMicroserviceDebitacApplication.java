35bcba18b13afa110ccd557bc908e40d8e1d8d65
Gaurav Tomar <gauravworld.07@gmail.com>
2019-02-18 21:40:23 +0530

    Updated import

--- src/main/java/org/payment/debitsc/PaymentMicroserviceDebitacApplication.java
+++ src/main/java/org/payment/debitsc/PaymentMicroserviceDebitacApplication.java
@@ -2,6 +2,7 @@ package org.payment.debitsc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.payment.debitsc.controller")
