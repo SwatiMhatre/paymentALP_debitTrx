e4acd5982ee2ad843b64f77a29f4e4ef6ec14e78
Gaurav Tomar <gauravworld.07@gmail.com>
2019-02-18 21:58:20 +0530

    Updated mappings

--- src/main/java/org/payment/debitsc/controller/DebitAcController.java
+++ src/main/java/org/payment/debitsc/controller/DebitAcController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(value="/debitac")
 public class DebitAcController {
 	@Autowired
 	DebitAccountRepository repository;
